74b9367e764cfc82d99deafaf3bb85899714165c
Mohammed Azharuddin <reachazhar@hotmail.com>
2026-02-01 12:15:53 -0800

    Revert "Add admin watchlist page for managing stocks to scan"
    This reverts commit 5e3c681f1ed29b3e33fa93da1556f4f4ff68b779.

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,4 +3,3 @@ export { Dashboard } from './Dashboard';
 export { AdminDashboard } from './AdminDashboard';
 export { AdminSystemInfo } from './AdminSystemInfo';
 export { AdminHealthCheck } from './AdminHealthCheck';
-export { AdminWatchlist } from './AdminWatchlist';
